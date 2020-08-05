4853e668b2d6269c2ea566782ad90a322b12ba69
Aditya <adi_bhutani16@yahoo.in>
2020-08-05 21:43:16 +0530

    another implemenation of heatmap with single query

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -1,7 +1,8 @@
 class HeatmapController < ApplicationController
   def index
     params[:dimension] = "department" if Employee.column_names.exclude? params[:dimension]
-    result = HeatMap.department_dimension(params[:dimension])
+    # result = HeatMap.department_dimension(params[:dimension])
+    result = HeatMap.department_dimension_2(params[:dimension])
     render json: result
   end
 end
--- app/services/heat_map.rb
+++ app/services/heat_map.rb
@@ -12,4 +12,31 @@ class HeatMap
     end
     result
   end
+
+  def self.department_dimension_2(dimension)
+    results = Response.select("responses.driver_name as driver, employees.#{dimension} as dimension, responses.score as score").joins(:employee)
+    arr = []
+
+    results.each do |data|
+      driver = arr.find{|d| d[:driver] == data.driver}
+      if driver.nil?
+        _out = { driver: data.driver, scores: {} }
+        _out[:scores][data.dimension] = [data.score.to_i]
+        arr << _out
+      else
+        if driver[:scores][data.dimension].present?
+          driver[:scores][data.dimension] << data.score.to_i
+        else
+          driver[:scores][data.dimension] = [data.score.to_i]
+        end
+      end
+    end
+
+    arr.each do |arr_element|
+      arr_element[:scores].each do |key, val|
+        arr_element[:scores][key] = (val.sum.to_f/val.size).truncate(2)
+      end
+    end
+    arr
+  end
 end
